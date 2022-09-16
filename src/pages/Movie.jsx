df3ed4cb226e2f8d4b6d606a1551d83b71cfb794
Pedro Lucas <79203933+pedrocslucas@users.noreply.github.com>
2022-09-16 11:53:38 -0300

    finalizando o search

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,3 +1,16 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+    BsGraphUp,
+    BsWallet2,
+    BsHourglassSplit,
+    BsFillFileEarmarkTextFill
+} from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+
+import "../styles/Movie.css";
+
 const Movie = () => {
     return (
         <div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -14,6 +14,19 @@ const Search = () => {
     const [movies, setMovies] = useState([]);
     const query = searchParams.get("q");
 
+    const getSearchedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setMovies(data.results);
+    };
+
+    useEffect(() => {
+        const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+
+        getSearchedMovies(searchWithQueryURL);
+    }, [query]);
+
     return (
         <div className="container">
             <h2 className="title">
